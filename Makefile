11e8a4db32cbd1967480ae7a74ff51e7b130ceff
robin <patrice@tisserand.org>
2013-04-13 16:17:37 +0800

    Initial import

---
+++ Makefile
@@ -0,0 +1,6 @@
+ARDUINO_DIR=/opt/arduino/arduino-1.0.3
+BOARD=mega2560
+BOARD_TAG=mega2560
+ARDUINO_PORT=/dev/ttyACM0
+ARDUINO_LIBS=IRremote
+include /usr/share/arduino/Arduino.mk
---
+++ irled.ino
@@ -0,0 +1,57 @@
+#include <IRremote.h>
+
+int RECV_PIN = 9;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+long MASK = 0x40BF0000; /* mask for remote */
+long key_mask = 0xFFFF;
+long val = 0;
+
+/* For debug */
+char* key_names[] = {
+  "UP",
+  "DOWN", 
+  "LEFT",
+  "RIGHT",
+  "OK"
+};
+
+long keys[] = {
+  0xB04F, /* UP */
+  0xF20D, /* DOWN */
+  0x728D, /* LEFT */
+  0x30CF, /* RIGHT */
+  0x9867, /* OK */
+};
+
+int nb_elem = sizeof(keys) / sizeof(keys[0]);
+int index = 0;
+
+void setup()
+{
+  Serial1.begin(9600);
+  irrecv.enableIRIn(); // Start the receiver
+  Serial1.println("GO ");
+}
+
+void loop() {
+  char* key_name = NULL;
+  if (irrecv.decode(&results)) {
+    if ((results.value >> 16)& (MASK >> 16)) {
+      key_name = NULL;
+      val = results.value & key_mask;
+      for (index = 0; index < nb_elem; ++index) {
+	if (keys[index] == val) {
+	  key_name = key_names[index];
+	  break;
+	}
+      }
+      if (NULL != key_name) {
+	Serial1.print("Received: ");
+	Serial1.println(key_name);
+      }
+    }
+    irrecv.resume(); // Receive the next value
+  }
+}
